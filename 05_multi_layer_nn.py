ee96a3da35cb1dc69bd8b2bb6ccd56c8d8161adf
Kaidoko <prashantyadavdec@gmail.com>
2026-04-27 07:29:02 +0530

    learned multi-layer neural network and created my first nn for curves and complex model to learn pattern through it and understood use of activation function

--- 05_multi_layer_nn.py
+++ 05_multi_layer_nn.py
@@ -1,19 +1,52 @@
+# Multi-layer model
+# that can learn complex patterns
+
+# Why we need more than linear? because linear cannot learn curves, patterns like XOR, complex decision boundaries
+# Solution -> stack layers
+
+# Basic NN Structure ->
+# Input → Linear → ReLU → Linear → Output
+
+# Building first NN
+
+import torch
 import torch.nn as nn
 
-model = nn.Linear(1, 1) # automatically handles weights and bias
+model = nn.Sequential(
+    nn.Linear(1, 10), # input -> hidden
+    nn.ReLU(), # activation function -> introduce non-linearity, lets model learn curves, complex mappings
+    nn.Linear(10, 1) # hidden -> output
+)
+
+# in this we have inserted 1 input value, 10 neurons in hidden layer and 1 output layer
+
+
+# Training Loop (same structure)
+
+
+x = torch.linspace(-2, 2, 100)
+y = x**2   # curve
+
 
 loss_fn = nn.MSELoss()
-optimzer = torch.optim.SGD(model.paramters(), lr = 0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
-for epoch in range(100):
+for epoch in range(200):
 
     y_pred = model(x.view(-1, 1))
 
     loss = loss_fn(y_pred, y.view(-1, 1))
 
-    optimzer.zero_grad()
+    optimizer.zero_grad()
     loss.backward()
-    optimzer.step()
+    optimizer.step()
 
-    if epoch % 10 == 0:
+    if epoch % 20 == 0:
         print(loss.item())
+
+
+# Key Understanding ->
+# Layer -> transformation
+# Activation -> adds complexity
+# Stack layers -> learn patterns
+
